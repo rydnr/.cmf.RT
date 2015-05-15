03a5a4d02fe6602e936ab9cd9496cb8d520e58f7
chous <chous@acm-sl.org>
2015-05-15 10:54:28 +0200

    201505151054

--- learn/mod_learn.c
+++ learn/mod_learn.c
@@ -122,6 +122,10 @@ static void print_headers_in(request_rec *r) {
     print_string("r->headers_in", "TODO", r);
 }
 
+static void print_headers_out(request_rec *r) {
+    print_string("r->headers_out", "TODO", r);
+}
+
 static char * itoa(int value) {
     int LENGTH = (CHAR_BIT * sizeof(int) - 1) / 3 + 2;
     char result[LENGTH];
@@ -187,6 +191,7 @@ static int learn_handler(request_rec *r)
         print_remaining(r);
         print_read_length(r);
         print_headers_in(r);
+        print_headers_out(r);
         
         print_string("r->content_type", r->content_type, r);
         print_string("r->content_encoding", r->content_encoding, r);
